e3774f1ba0e783c53f0602d964a96bf38c0d3441
jorihuamancha <jorihuamancha@hotmail.com>
2014-09-11 20:53:25 -0300

    Bundle creado

---
+++ src/Cresta/AulasBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cresta\AulasBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('CrestaAulasBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
