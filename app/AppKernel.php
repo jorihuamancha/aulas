a7387dc157c6e6006f2ee3bea28c913d840e94db
Nicolas Menna <nicolas.menna90@gmail.com>
2014-10-29 16:04:18 -0300

    instalacion de pdfbundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Cresta\AulasBundle\CrestaAulasBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Ps\PdfBundle\PsPdfBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
