16c17e1819ed82b5ec8af48d408345de935202e6
Jori Huamancha <jorihuamancha@hotmail.com>
2014-10-30 17:26:15 -0300

    antes de mergear con la rama de Ale

--- src/Cresta/AulasBundle/Controller/DefaultController.php
+++ src/Cresta/AulasBundle/Controller/DefaultController.php
@@ -3,6 +3,11 @@
 namespace Cresta\AulasBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Ps\PdfBundle\Annotation\Pdf;
+
+/**
+* @Pdf()
+*/
 
 class DefaultController extends Controller
 {
@@ -20,4 +25,14 @@ class DefaultController extends Controller
     {
         return $this->render('CrestaAulasBundle:Default:ayuda.html.twig', array());
     }
+
+    public function imprimirAction(){
+        $formato=$this->get('request')->get('_format');
+        return $this->render(sprintf('CrestaAulasBundle:Default:lista.pdf.twig', $formato ),  array() );
+    }
+
+	/*public function imprimirAction($listado){
+		$formato=$this->get('request')->get('_format');
+		return $this->render(sprintf('AulasBundle:DefaultController:lista.%s.twig', $formato), array('listado'=>$listado ));
+	}*/
 }
