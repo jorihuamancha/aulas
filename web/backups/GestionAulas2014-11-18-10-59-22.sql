75879b39770f12961436a1236c92a122a031d3bf
Qw3r7y <cabanehb@gmail.com>
2014-11-19 02:23:56 -0300

    arreglado select dia de disponibilidad de aulas, y detalles menores

---
+++ web/backups/GestionAulas2014-11-18-10-59-22.sql
@@ -0,0 +1,348 @@
+-- MySQL dump 10.13  Distrib 5.5.40, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: symfony
+-- ------------------------------------------------------
+-- Server version	5.5.40-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Actividad`
+--
+
+DROP TABLE IF EXISTS `Actividad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Actividad` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `tipo` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Actividad`
+--
+
+LOCK TABLES `Actividad` WRITE;
+/*!40000 ALTER TABLE `Actividad` DISABLE KEYS */;
+INSERT INTO `Actividad` VALUES (1,'Ejercicio con el PEMTA','Conferencia');
+/*!40000 ALTER TABLE `Actividad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Administrador`
+--
+
+DROP TABLE IF EXISTS `Administrador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Administrador` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `apellido` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Administrador`
+--
+
+LOCK TABLES `Administrador` WRITE;
+/*!40000 ALTER TABLE `Administrador` DISABLE KEYS */;
+INSERT INTO `Administrador` VALUES (1,'Alejandro','Cabane',1);
+/*!40000 ALTER TABLE `Administrador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Alerta`
+--
+
+DROP TABLE IF EXISTS `Alerta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Alerta` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` date NOT NULL,
+  `descripcion` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `observaciones` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Alerta`
+--
+
+LOCK TABLES `Alerta` WRITE;
+/*!40000 ALTER TABLE `Alerta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Alerta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Aula`
+--
+
+DROP TABLE IF EXISTS `Aula`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Aula` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `piso` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `capacidad` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Aula`
+--
+
+LOCK TABLES `Aula` WRITE;
+/*!40000 ALTER TABLE `Aula` DISABLE KEYS */;
+INSERT INTO `Aula` VALUES (1,'101','1',35,1),(2,'103','1',35,1),(3,'102','1',35,1);
+/*!40000 ALTER TABLE `Aula` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Carrera`
+--
+
+DROP TABLE IF EXISTS `Carrera`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Carrera` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `observaciones` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Carrera`
+--
+
+LOCK TABLES `Carrera` WRITE;
+/*!40000 ALTER TABLE `Carrera` DISABLE KEYS */;
+INSERT INTO `Carrera` VALUES (1,'Analista Programador Universitario','La Plata');
+/*!40000 ALTER TABLE `Carrera` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Curso`
+--
+
+DROP TABLE IF EXISTS `Curso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Curso` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `anio` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `Carrera_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_BFA6FE8892CB7DF` (`Carrera_id`),
+  CONSTRAINT `FK_BFA6FE8892CB7DF` FOREIGN KEY (`Carrera_id`) REFERENCES `Carrera` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Curso`
+--
+
+LOCK TABLES `Curso` WRITE;
+/*!40000 ALTER TABLE `Curso` DISABLE KEYS */;
+INSERT INTO `Curso` VALUES (1,'Curso','2004',NULL),(2,'Beia Primavera','2004',1);
+/*!40000 ALTER TABLE `Curso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Docente`
+--
+
+DROP TABLE IF EXISTS `Docente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Docente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `apellido` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Docente`
+--
+
+LOCK TABLES `Docente` WRITE;
+/*!40000 ALTER TABLE `Docente` DISABLE KEYS */;
+INSERT INTO `Docente` VALUES (1,'Jorge','Runco',1);
+/*!40000 ALTER TABLE `Docente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Movimiento`
+--
+
+DROP TABLE IF EXISTS `Movimiento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Movimiento` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` date NOT NULL,
+  `reservaMovimiento_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_87A213AA72257DB9` (`reservaMovimiento_id`),
+  CONSTRAINT `FK_87A213AA72257DB9` FOREIGN KEY (`reservaMovimiento_id`) REFERENCES `Reserva` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Movimiento`
+--
+
+LOCK TABLES `Movimiento` WRITE;
+/*!40000 ALTER TABLE `Movimiento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Movimiento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Recurso`
+--
+
+DROP TABLE IF EXISTS `Recurso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Recurso` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Recurso`
+--
+
+LOCK TABLES `Recurso` WRITE;
+/*!40000 ALTER TABLE `Recurso` DISABLE KEYS */;
+INSERT INTO `Recurso` VALUES (1,'Cañon',1);
+/*!40000 ALTER TABLE `Recurso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Reserva`
+--
+
+DROP TABLE IF EXISTS `Reserva`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Reserva` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cursos_id` int(11) DEFAULT NULL,
+  `actividades_id` int(11) DEFAULT NULL,
+  `recursos_id` int(11) DEFAULT NULL,
+  `estado` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `observaciones` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `fechaRegistro` date NOT NULL,
+  `horaDesde` time NOT NULL,
+  `horaHasta` time NOT NULL,
+  `fechaReserva` date NOT NULL,
+  `reservaAula_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_D73017A76AFE7B9A` (`cursos_id`),
+  KEY `IDX_D73017A72F4F3E2F` (`actividades_id`),
+  KEY `IDX_D73017A776F0FADE` (`recursos_id`),
+  KEY `IDX_D73017A729BEB574` (`reservaAula_id`),
+  CONSTRAINT `FK_D73017A729BEB574` FOREIGN KEY (`reservaAula_id`) REFERENCES `Aula` (`id`),
+  CONSTRAINT `FK_D73017A72F4F3E2F` FOREIGN KEY (`actividades_id`) REFERENCES `Actividad` (`id`),
+  CONSTRAINT `FK_D73017A76AFE7B9A` FOREIGN KEY (`cursos_id`) REFERENCES `Curso` (`id`),
+  CONSTRAINT `FK_D73017A776F0FADE` FOREIGN KEY (`recursos_id`) REFERENCES `Recurso` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Reserva`
+--
+
+LOCK TABLES `Reserva` WRITE;
+/*!40000 ALTER TABLE `Reserva` DISABLE KEYS */;
+INSERT INTO `Reserva` VALUES (3,1,1,1,'asd','asd','2014-11-04','08:30:00','10:30:00','2014-11-25',1),(4,1,1,1,'','','2014-11-18','10:00:00','14:30:00','2014-11-25',3),(5,1,1,1,'','','2014-11-18','11:30:00','14:30:00','2014-11-25',1),(6,1,1,1,'','','2014-11-18','15:00:00','15:30:00','2014-11-25',2);
+/*!40000 ALTER TABLE `Reserva` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Usuario`
+--
+
+DROP TABLE IF EXISTS `Usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `username_canonical` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email_canonical` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `salt` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `locked` tinyint(1) NOT NULL,
+  `expired` tinyint(1) NOT NULL,
+  `expires_at` datetime DEFAULT NULL,
+  `confirmation_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password_requested_at` datetime DEFAULT NULL,
+  `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
+  `credentials_expired` tinyint(1) NOT NULL,
+  `credentials_expire_at` datetime DEFAULT NULL,
+  `docente_id` int(11) DEFAULT NULL,
+  `administrador_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_EDD889C192FC23A8` (`username_canonical`),
+  UNIQUE KEY `UNIQ_EDD889C1A0D96FBF` (`email_canonical`),
+  UNIQUE KEY `UNIQ_EDD889C194E27525` (`docente_id`),
+  UNIQUE KEY `UNIQ_EDD889C148DFEBB7` (`administrador_id`),
+  CONSTRAINT `FK_EDD889C148DFEBB7` FOREIGN KEY (`administrador_id`) REFERENCES `Administrador` (`id`),
+  CONSTRAINT `FK_EDD889C194E27525` FOREIGN KEY (`docente_id`) REFERENCES `Docente` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Usuario`
+--
+
+LOCK TABLES `Usuario` WRITE;
+/*!40000 ALTER TABLE `Usuario` DISABLE KEYS */;
+INSERT INTO `Usuario` VALUES (1,'admin','admin','cabanehb@gmail.com','cabanehb@gmail.com',1,'a44rshpdir4soggskwo0ocws4k4o84o','k2p8+0+va9978MxvYav/2pCeRb91BWVV7oCN7sGtEtTqwHSyQ/2QD8V8510V1eXktf8XcYbDcKKiUME9f8JIzA==','2014-11-18 10:22:21',0,0,NULL,NULL,NULL,'a:1:{i:0;s:16:\"ROLE_SUPER_ADMIN\";}',0,NULL,NULL,1),(4,'jrunco','jrunco','runco@fisica.unlp.edu.ar','runco@fisica.unlp.edu.ar',1,'pkwy469expcg0gg88cgg848soo0ggs','N4uWyPqy/z0ARrJtaINITVDjHc96zKz1dAz+Y1lurS1/eBjIin7+F4Zl0qI77P3+WkM1MBDbw6JoEyZgUboydQ==',NULL,0,0,NULL,NULL,NULL,'a:0:{}',0,NULL,1,NULL);
+/*!40000 ALTER TABLE `Usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-11-18 10:59:23
