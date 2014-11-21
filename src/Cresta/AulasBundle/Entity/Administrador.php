9c9f7cd77a12c4ab898fcc3f97a3a0eeec549d6a
nmenna10 <nicolas.menna90@gmail.com>
2014-11-21 19:07:05 -0300

    asdasdasd

---
+++ src/Cresta/AulasBundle/Entity/Administrador.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Cresta\AulasBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Docente
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Administrador extends Persona
+{
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="activo", type="boolean")
+     */
+    private $activo;
+
+    private $personaAdministrador;
+
+    /**
+     * Set activo
+     *
+     * @param boolean $activo
+     * @return Docente
+     */
+    public function setActivo($activo)
+    {
+        $this->activo = $activo;
+
+        return $this;
+    }
+
+    /**
+     * Get activo
+     *
+     * @return boolean 
+     */
+    public function getActivo()
+    {
+        return $this->activo;
+    }
+
+    /**
+     * Get activo
+     *
+     * @return boolean 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    public function getPersonaAdministrador(){
+        return $this->personaAdministrador;
+    }
+
+}
