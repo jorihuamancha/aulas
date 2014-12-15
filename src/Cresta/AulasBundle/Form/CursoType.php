503000207c8266dd7becb5577bfb6d05e045417a
Qw3r7y <cabanehb@gmail.com>
2014-12-15 01:23:48 -0300

    arreglos menores, todo lo del email OK, falta lo de nueva reserva

--- src/Cresta/AulasBundle/Form/CursoType.php
+++ src/Cresta/AulasBundle/Form/CursoType.php
@@ -15,8 +15,8 @@ class CursoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('anio','text',array('label'=>'Año:'))
             ->add('nombre','text',array('label'=>'Nombre:'))
+            ->add('anio','text',array('label'=>'Año:'))
             ->add('Carrera','entity',array('class'=>'CrestaAulasBundle:Carrera','property'=>'nombre','label'=>'Carrera:'))
         ;
     }
