9afdbb67ea7741ca53de4791e320b58fd8f39311
Neg.- <neg90.ng@gmail.com>
2015-03-03 23:02:26 -0300

    jori la quiere toda

--- src/Cresta/AulasBundle/Controller/ReservaController.php
+++ src/Cresta/AulasBundle/Controller/ReservaController.php
@@ -56,6 +56,24 @@ class ReservaController extends Controller
         ));
     }
 
+    private function agregarEnCascada ($entity) {
+       
+        $em = $this->getDoctrine()->getManager();
+        
+        $fecha=$entity->getFecha();
+     
+        $fecha->modify('+1 day');
+
+        $entity->setFecha($fecha);
+
+       // $this->createAction();
+        
+
+        //Por las dudas.
+        return $entity;
+        
+    }
+
     /**
      * Creates a new Reserva entity.
      *
@@ -86,8 +104,9 @@ class ReservaController extends Controller
                 $fechaActual=new \DateTime('now');
                 $fechaActual->setTime(00, 00, 00);
 
-
-                if ()
+                if ($entity->getRango() > 1){
+                    $hayVariasCargas = true;
+                }
 
                 if (!($entity->getFecha()>=$fechaActual)) {
                     throw new Exception("La fecha para reservar deberia ser mas grande que la fecha actual.");  
@@ -97,25 +116,28 @@ class ReservaController extends Controller
                     throw new Exception("Hay una alerta activa para el dia que desea agregar una reserva.");
                 }elseif (!($this->sePuede($entity))) {
                     throw new Exception("Hay reservas para esa aula con esas fecha y hora");
-                }
+                }  
+
+
+                if ($hayVariasCargas)
+                    while ($entity->getrangoHasta() >= $entity->getFecha()) 
+                        $this->agregarEnCascada($entity);
 
                 try{
-                //if(($entity->getFecha()>=$fechaActual)&&($entity->getHoraDesde()<$entity->getHoraHasta())){
                     $em->persist($entity);
                     $em->flush();
                     
                 }catch(Exception $e){
-                //}
-                    //$e->getMessage();
-                //throw new Exception("Compruebe los campos de las fechas y las horas de la reserva.");
+               
                 }
+              
 
             return $this->redirect($this->generateUrl('reserva_show', array('id' => $entity->getId())));
             }
-        return $this->render('CrestaAulasBundle:Reserva:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView()
-        ));
+       return $this->render('CrestaAulasBundle:Reserva:new.html.twig', array(
+           'entity' => $entity,
+           'form'   => $form->createView()
+      ));
     }
 
     private function conprobarAlerta ($fecha){
